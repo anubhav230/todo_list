b8802b13edcb99f8b26f873027f403e9df433681
Anubhav <anubhav@spalba.com>
2021-09-23 03:52:54 +0530

    ADDED: form for creating list

--- src/component/todo_list.js
+++ src/component/todo_list.js
@@ -1,11 +1,78 @@
 import React, { Component } from 'react';
+import {
+    Card, CardHeader, CardBody,
+    Row,
+    Col
+
+} from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.css';
 class ActiveEvents extends Component {
+    constructor(props) {
+        super(props);
+        this.columns = [{
+            dataField: 'id',
+            text: 'ID',
+        }];
+
+        this.state = {
+            todo_list: [],
+        }
+    }
     render() {
-        return(
-            <div>
-                <header className="App-header">
-                    <lable> welcom to todo list by anubhav singh </lable>
-                </header>  
+        return (
+            <div style={{ backgroundColor: '' }}>
+                <div style={{ width: '30%' }}>
+                    <Card>
+                        <CardHeader></CardHeader>
+                        <CardBody></CardBody>
+                    </Card>
+                    <Row>
+                        <form id="create-form">
+                            <Col id="spaces" md="8">
+                                <div className="form-group mb-2">
+                                    <span>Title: </span>
+                                    <input class="form-control" type="text"
+                                    />
+                                    <small style={{ color: 'red' }}></small>
+                                </div>
+                            </Col>
+                            <Col id="spaces" md="8">
+                                <div className="form-group mb-2">
+                                    <span>Desvription: </span>
+                                    <input class="form-control" type="text"
+                                    />
+                                    <small style={{ color: 'red' }}></small>
+                                </div>
+                            </Col>
+                            <Col id="spaces" md="8">
+                                <div className="form-group mb-2">
+                                    <span>Due date: </span>
+                                    <input class="form-control" type="text"
+                                    />
+                                    <small style={{ color: 'red' }}></small>
+                                </div>
+                            </Col>
+                            <Col id="spaces" md="8">
+                                <div className="form-group mb-2">
+                                    <span>Tag </span>
+                                    <input class="form-control" type="text"
+                                    />
+                                    <small style={{ color: 'red' }}></small>
+                                </div>
+                            </Col>
+                            <Col id="spaces" md="8">
+                                <div className="form-group mb-2">
+                                    <span>Status </span>
+                                    <input class="form-control" type="text"/>
+                                    <small style={{ color: 'red' }}></small>
+                                </div>
+                            </Col>
+                        </form>
+                    </Row>
+                    <button className="btn btn-primary btn-sm radius-md"><span>Submit</span></button>
+                </div>
+                <div>
+                </div>
             </div>
         )
     }
