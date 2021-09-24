14dcda0c8c1fa3fb8b3da314663ce4bab91d2055
Anubhav <anubhav@spalba.com>
2021-09-25 03:47:24 +0530

    css changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import Router from './router'
 
 function App() {
--- src/component/todo_list.js
+++ src/component/todo_list.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import {
-    Card, CardHeader, CardBody,
-    Row,
-    Col,
+    CardHeader, CardBody,
     UncontrolledDropdown,
     DropdownMenu,
     DropdownToggle
@@ -11,6 +9,9 @@ import {
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
+import { Card, Button, Input, Select, DatePicker, Row, Col, Modal } from 'antd'
+import 'antd/dist/antd.css';
+import { PoweroffOutlined } from '@ant-design/icons';
 class ActiveEvents extends Component {
     constructor(props) {
         super(props);
@@ -41,6 +42,11 @@ class ActiveEvents extends Component {
                 sort: true
             },
             {
+                dataField: 'tag_id',
+                text: 'Tags',
+                sort: true
+            },
+            {
                 dataField: 'link',
                 text: 'Action',
                 formatter: this.actionFormatUser.bind(this)
@@ -48,27 +54,44 @@ class ActiveEvents extends Component {
         ]
 
         this.state = {
+            dateFormat: 'YYYY-MM-DD',
             title: '',
             description: '',
             due_date: '',
             timestamp: '',
-            status: 'OPEN',
-
+            status: '',
             data: {},
             todo_list: [],
             title_error: '',
             description_error: '',
             status_error: '',
             isEdit: false,
-            editData: {} 
+            editData: {},
+            show: false,
+            tags: [
+                {
+                    id: 1,
+                    name: 'sunday',
+                },
+                {
+                    id: 2,
+                    name: 'monday',
+                },
+                {
+                    id: 3,
+                    name: 'test',
+                },
+            ]
 
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.validation = this.validation.bind(this);
-
-
-    }
-
+        this.handleSelect = this.handleSelect.bind(this);
+        this.handleDateSelect = this.handleDateSelect.bind(this);
+        this.showModal = this.showModal.bind(this);
+        this.handleOk = this.handleOk.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+    }  
     actionFormatUser(rowContent, row) {
 
         return (
@@ -95,14 +118,15 @@ class ActiveEvents extends Component {
             due_date: data.due_date,
             status: data.status,
             isEdit: true,
-            editData: data
+            editData: data,
+
         })
     }
 
     delete(data) {
         const { todo_list } = this.state;
         todo_list.map((item, index) => {
-            if(item.id == data.id) {
+            if (item.id === data.id) {
                 todo_list.splice(index, 1);
             }
         })
@@ -141,7 +165,7 @@ class ActiveEvents extends Component {
         if (isSubmit) {
             if (isEdit) {
                 todo_list.map((item, index) => {
-                    if(item.id == editData.id) {
+                    if (item.id === editData.id) {
                         todo_list[index] = data;
                     }
                 });
@@ -155,79 +179,121 @@ class ActiveEvents extends Component {
         }
     }
 
-    render() {
-        const { title_error, description_error, status_error, todo_list } = this.state;
-        return (
-            <div>
-                <h2 style={{ textAlign: 'center' }}> TODO List </h2>
-                <div style={{ padding: '10px', display: 'flex' }}>
-                    <div style={{ width: '30%' }}>
-                        <Card>
-                            <CardHeader></CardHeader>
-                            <CardBody></CardBody>
-                        </Card>
-                        <Row>
-                            <form id="create-form">
-                                <Col id="spaces" md="8">
-                                    <div className="form-group mb-2">
-                                        <span>Title: </span>
+    switchEl(el, id) {
+        const { todo_list } = this.state;
+        if (todo_list.el && todo_list[el].includes(id)) todo_list[el] = todo_list[el].filter(r => r !== id);
+        else todo_list[el].push(id);
+        this.setState({ todo_list });
+    }
+    handleSelect(value) {
+        console.info('ssssss', value)
+        this.setState({ status: value })
+    }
+    handleDateSelect(date, stringDate) {
+        console.info(stringDate)
+        this.setState({ due_date: stringDate })
+    }
+    showModal() {
+        this.setState({ show: true })
+    }
+    handleCancel() {
+        this.setState({show: false})
+    }
 
-                                        <input type="text" class="form-control"
-                                            maxLength='100'
-                                            value={this.state.title && this.state.title}
-                                            onChange={e => this.setState({ title: e.target.value })}
-                                        />
-                                        <small style={{ color: 'red' }}>{title_error}</small>
-                                    </div>
-                                </Col>
-                                <Col id="spaces" md="8">
-                                    <div className="form-group mb-2">
-                                        <span>Description: </span>
-                                        <textarea class="form-control" type="text"
-                                            maxLength='1000'
-                                            value={this.state.description && this.state.description}
-                                            onChange={e => this.setState({ description: e.target.value })}
-                                        />
-                                        <small style={{ color: 'red' }}>{description_error}</small>
-                                    </div>
-                                </Col>
-                                <Col id="spaces" md="8">
-                                    <div className="form-group mb-2">
-                                        <span>Due date: </span>
-                                        <input class="form-control" type="date"
-                                            value={this.state.due_date && this.state.due_date}
-                                            onChange={e => this.setState({ due_date: e.target.value })}
-                                        />
-                                        <small style={{ color: 'red' }}></small>
-                                    </div>
-                                </Col>
-                                <Col id="spaces" md="8">
-                                    <div className="form-group mb-2">
-                                        <span>Tag </span>
-                                        <input class="form-control" type="text"
-                                            onChange={e => this.setState({ title: e.target.value })}
-                                        />
-                                    </div>
-                                </Col>
-                                <Col id="spaces" md="8">
-                                    <div className="form-group mb-2">
-                                        <span>Status </span>
-                                        <select class="form-control" type="text"
-                                            value={this.state.status && this.state.status}
-                                            onChange={e => this.setState({ status: e.target.value })}
-                                        >
-                                            <option value=''>SELECT</option>
-                                            <option>OPEN</option>
-                                            <option>WORKING</option>
-                                            <option>DONE</option>
-                                            <option>OVERDUE</option>
-                                        </select>
-                                        <small style={{ color: 'red' }}>{status_error}</small>
-                                    </div>
-                                </Col>
-                            </form>
-                        </Row>
-                        <button className="btn btn-primary btn-sm radius-md" onClick={this.handleSubmit}><span>Submit</span></button>
+    handleOk() {
+        console.info('ookokoko')
+        this.setState({show: false})
+    }
+
+    render() {
+        const { title_error, description_error, status_error, todo_list, show } = this.state;
+        const { TextArea } = Input;
+        const { Option } = Select;
+        return (
+            <div style={{ backgroundColor: 'Lavender' }}>
+                <Row style={{ backgroundColor: 'DarkGrey' }}>
+                    <Col span={24}><h2 style={{ color: 'white' }}>TODO List</h2></Col>
+                </Row>
+                <Modal title="Basic Modal" visible={show} onOk={this.handleOk}  onCancel={this.handleCancel}>
+                </Modal>
+                <div style={{ padding: '30px', display: 'flex' }}>
+
+                    <div style={{ width: '30%' }}>
+                        <Card title="Create List" style={{ width: 400 }}>
+                            <Row>
+                                <form>
+                                    <Col>
+                                        <div>
+                                            <span>Title: </span>
+                                            <Input type="text"
+                                                maxLength='100'
+                                                value={this.state.title && this.state.title}
+                                                onChange={e => this.setState({ title: e.target.value })}
+                                            />
+                                            <small style={{ color: 'red' }}>{title_error}</small>
+                                        </div>
+                                    </Col>
+                                    <br />
+                                    <Col>
+                                        <div>
+                                            <span>Description: </span>
+                                            <TextArea class="form-control" type="text"
+                                                maxLength='1000'
+                                                value={this.state.description && this.state.description}
+                                                onChange={e => this.setState({ description: e.target.value })}
+                                            />
+                                            <small style={{ color: 'red' }}>{description_error}</small>
+                                        </div>
+                                    </Col>
+                                    <br />
+                                    <Col>
+                                        <div>
+                                            <span>Due date: </span>
+                                            <br />
+                                            <DatePicker
+                                                style={{ width: 350 }}
+                                                defaultValue={this.state.due_date && ''}
+                                                onChange={this.handleDateSelect}
+                                            />
+                                            <small style={{ color: 'red' }}></small>
+                                        </div>
+                                    </Col>
+                                    <br />
+                                    <Col>
+                                        <div>
+                                            <span>Tag </span><Button size='small' type="primary" onClick={this.showModal}>+</Button>
+                                            <br />
+                                            <br />
+                                            <Select
+                                                style={{ width: 350 }}
+                                                onChange={this.handleSelect}
+                                            />
+                                        </div>
+                                    </Col>
+                                    <br />
+                                    <Col>
+                                        <div>
+                                            <span>Status </span>
+                                            <br />
+                                            <Select
+                                                placeholder="Select a status"
+                                                style={{ width: 350 }}
+                                                onChange={this.handleSelect}
+                                            >
+                                                <Option value="OPEN">OPEN</Option>
+                                                <Option value="WORKING">WORKING</Option>
+                                                <Option value="DONE">DONE</Option>
+                                                <Option value="OVERDUE">OVERDUE</Option>
+                                            </Select>
+                                            <small style={{ color: 'red' }}>{status_error}</small>
+                                        </div>
+                                    </Col>
+                                </form>
+                            </Row>
+                            <br />
+                            <Button type="primary" onClick={this.handleSubmit}>Button</Button>
+                        </Card>
+
                     </div>
                     <div style={{ width: '68%' }}>
                         <div className="mb-4">
